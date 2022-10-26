75e7217ae678b36ff72be10bc9f7b186666c9c46
Fevzi Sanal <fevzi.sanal999@gmail.com>
2022-10-26 21:04:22 +0300

    odev3

--- app_server/controllers/mekanlar.js
+++ app_server/controllers/mekanlar.js
@@ -1,12 +1,69 @@
 var express = require("express");
-var router = express.Router();
 
+/* GET home page. */
 const anaSayfa = function (req, res, next) {
-  res.render("anasayfa", { title: "Anasayfa" });
+  res.render("anasayfa", {
+    baslik: "Anasayfa",
+    sayfaBaslik: {
+      siteAd: "MekanBul",
+      slogan: "Civardaki Mekanları Keşfet!",
+    },
+    mekanlar: [
+      {
+        ad: "Starbucks",
+        puan: 4,
+        adres: "Centrum Garden AVM",
+        imkanlar: ["Dünya Kahveleri", "Kekler", "Pastalar"],
+        mesafe: "10km",
+      },
+      {
+        ad: "Gloria Jeans",
+        puan: 3,
+        adres: "Sdü Doğu Kampüsü",
+        imkanlar: ["Kahve", "Çay", "Pasta"],
+        mesafe: "5km",
+      },
+    ],
+  });
 };
 
 const mekanBilgisi = function (req, res, next) {
-  res.render("mekanbilgisi", { title: "Mekan Bilgisi" });
+  res.render("mekanbilgisi", {
+    baslik: "Mekan Bilgisi",
+    mekanBaslik: "Starbucks",
+    mekanDetay: {
+      ad: "Starbucks",
+      puan: "4",
+      adres: "Centrum Garden AVM",
+      saatler: [
+        {
+          gunler: "Pazartesi-Cuma",
+          acilis: "9:00",
+          kapanis: "23:00",
+          kapali: false,
+        },
+        {
+          gunler: "Cumartesi-Pazar",
+          acilis: "8:00",
+          kapanis: "22:00",
+          kapali: false,
+        },
+      ],
+      imkanlar: ["Dünya Kahveleri", "Kekler", "Pastalar"],
+      koordinatlar: {
+        enlem: 37.78,
+        boylam: 30.56,
+      },
+      yorumlar: [
+        {
+          yorumYapan: "Fevzi Şanal",
+          yorumMetni: "Kahveleri güzel.",
+          tarih: "23 Ekim 2022",
+          puan: "4",
+        },
+      ],
+    },
+  });
 };
 
 const yorumEkle = function (req, res, next) {
